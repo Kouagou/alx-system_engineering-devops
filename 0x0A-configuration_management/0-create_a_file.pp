b68627c9e7637babff17c8d47f8dc1ff76dbb08c
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-03-22 18:36:33 -0700

    Task 0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
 file { '/tmp/school':
-  path => '/tmp/school',
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744',
+  ensure  => 'file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
   content => 'I love Puppet',
-  ensure => 'present'
 }
