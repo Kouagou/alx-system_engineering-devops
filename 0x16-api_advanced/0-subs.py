159f0832b9ce9117b04d7bb0d8f9b9f24f7bbfda
Kouagou Kenneth <ncthakenneth@gmail.com>
2024-05-07 15:29:46 -0700

    Task 0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Quering the Reddit API and returns the number of subscribers. """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function that queries the Reddit API and returns the number of
+        subscribers (not active users, total subscribers) for a given
+        subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-agent': 'linux:0x16.api.advanced'
+    }
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    datas = res.json()
+
+    try:
+        return datas.get("subscribers")
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+Solutions for 0x16. API advanced
